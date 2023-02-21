1f4c0a1daf24d9ddc3e73122dd9b2891833ad535
morihisashi <hisashi.mori@humanage.co.jp>
2023-02-21 19:30:29 +0900

    useState,useEffect練習

--- webpage-react/src/pages/Homepage.js
+++ webpage-react/src/pages/Homepage.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import { useState, useEffect, React} from 'react';
 import { Body } from '../components/body';
+import '../App.css';
 
 export function Homepage(){
+  const [count, setCount] = useState(0)
+  const [count2, setCount2] = useState(0)
+  useEffect(()=>{
+    console.log('useEffectが実行されました')
+  },[count])
   return (
     <div>
       <Body title="Home Pages" name="homepageへ遷移" />
+      <div className={"App"}>
+      <h2>Count: { count }/ Count2: { count2 }</h2>
+      <button onClick={() => setCount(count+1)}>Count+</button><br/>
+      <button onClick={() => setCount2(count2+1)}>Count2+</button><br/>
+      </div>
     </div>
   );
 }
