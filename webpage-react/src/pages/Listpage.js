6e87f3d64ff620d021bc52b313be0b7366969452
morihisashi <hisashi.mori@humanage.co.jp>
2023-02-24 19:17:58 +0900

    削除機能実装中

--- webpage-react/src/pages/Listpage.js
+++ webpage-react/src/pages/Listpage.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import {React, useState }from 'react';
 import '../App.css';
 
 export function Listpage(){
     const str = sessionStorage.getItem('todoData');
     const array = str.split(',');
+    const [del, delText] = useState("");
+    const [delArrays, setDel] = useState(array);
+
+    const onClickDelText = (val) => {
+        delText({val});
+        setDel(
+            delArrays.filter((delArray, index) => (delArray !== del))
+        )
+        console.log(delArrays);
+        //sessionStorage['todoData'] = delArrays;
+    }
     return (
         <div className={"App"}>
             <h1>ToDo List</h1>
             <div className={"list"}>
-                {array.map((val) => <div className={"listItmes"}>{val}</div>)}
+                {array.map((val) => 
+                <div>
+                    <div className={"listItmes"}>{val}</div>
+                    <button onClick={() => onClickDelText(val)}>削除</button>
+                </div>
+                )}
             </div>
         </div>
     );
