21c891e15947d4520468cf52cef7214b7d180834
morihisashi <hisashi.mori@humanage.co.jp>
2023-02-27 11:36:32 +0900

    Listpage.jsをリファクタリング

--- webpage-react/src/pages/Listpage.js
+++ webpage-react/src/pages/Listpage.js
@@ -2,7 +2,7 @@ import {React, useState, useEffect }from 'react';
 import '../App.css';
 
 export function Listpage(){
-    const str= sessionStorage.getItem('todoData');
+    const str = sessionStorage.getItem('todoData');
     const [array, setArray]= useState(str.split(','));
 
     const onClickDelArray = (val) => {
