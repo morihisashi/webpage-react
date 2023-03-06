707c6a86fa1d69f021744ec36f88ad3e1c7091a8
morihisashi <hisashi.mori@humanage.co.jp>
2023-03-06 14:08:47 +0900

    Link先を編集

--- webpage-react/src/components/header.jsx
+++ webpage-react/src/components/header.jsx
@@ -6,11 +6,11 @@ export function Header(props){
     return (
         <div className={header.atag}>
             <p>headerコンポーネントの作成</p>
-            <Link to="/webpage-react" className={header.atag}>{props.array[0][0]}</Link>
-            <Link to="/webpage-react/list">todoの一覧へ</Link>
-            <Link to="/webpage-react/about" className={header.atag}>{props.array[0][1]}</Link>
-            <Link to="/webpage-react/practical" className={header.atag}>{props.array[0][2]}</Link>
-            <Link to="/webpage-react/practicallist">Practical List</Link>
+            <Link to="/" className={header.atag}>{props.array[0][0]}</Link>
+            <Link to="/list">todoの一覧へ</Link>
+            <Link to="/about" className={header.atag}>{props.array[0][1]}</Link>
+            <Link to="/practical" className={header.atag}>{props.array[0][2]}</Link>
+            <Link to="/practicallist">Practical List</Link>
         </div>
     );
 }
